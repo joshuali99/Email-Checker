79e346d8aa8b3907c241a874fd6eecf0eb9a6f49
Joshua Lee <joshuali99@gmail.com>
2020-05-14 22:41:34 -0400

    Second commit, with tracking numbers included.

--- emailchecker.py
+++ emailchecker.py
@@ -8,10 +8,11 @@ import datetime
 import pandas as pd
 import re
 import bestBuy
+import csv
 
 #set up the email connection in imap and imap_tools
-username = ""
-password = ""
+username = "INSERT EMAIL"
+password = "INSERT PASSWORD"
 imap = imaplib.IMAP4_SSL("imap.gmail.com")
 mailbox = MailBox("imap.gmail.com")
 imap.login(username, password)
@@ -20,37 +21,49 @@ status, numberMessages = imap.select()
 
 #date from which we want the emails
 year = 2020
-day = 10
+day = 8
 month = 5
 
 #fetches Best Buy emails
 bestBuyMessages = mailbox.fetch(Q(from_="BestBuyInfo@emailinfo.bestbuy.com", date_gte=datetime.date(year,month,day)))
 
 #creates output dataframe that will be added to
-output = pd.DataFrame(index = ['BBY01'], columns = ['Shipping #', 'Date', 'Product # Quantity', 'Destination'] )
-currentOrders = pd.read_csv(r'orderConfirmations.csv')
+orderConfirmations = pd.DataFrame(index = ['BBY01'], columns = ['Shipping #', 'Date', 'Product # Quantity', 'Destination', 'Cost'])
+shippedOrders = pd.DataFrame(index = ['BBY01'], columns = ['Shipping #', 'Date', 'Product # Quantity', 'Destination', 'Cost'] )
+#orderConfirmations = pd.read_csv(r'orderConfirmations.csv', index_col = 0)
+#shippedOrders = pd.read_csv(r'shippedOrders.csv')
 
 # loops through these Best Buy emails
 for msg in bestBuyMessages:
-    #    message.subject          # str: 'some subject'
     
     dateReceived = msg.date_str
     msgHtml = msg.html
 
     #Checks if it is a "received order," so no shipping # yet.
     if "We've received your order" in msg.subject:
-
+        #assigns the values of model/quantity, destination, and order number
         modelAndQuantity = str(bestBuy.findModelAndQuantity(msgHtml))
         destination = bestBuy.findShippingLocation(msgHtml)
         orderNo = bestBuy.findOrderNo(msgHtml)
+        cost = bestBuy.findOrderTotal(msgHtml)
 
-        append = pd.DataFrame({'Shipping #': None, 'Date': dateReceived, 'Product # Quantity': modelAndQuantity, 'Destination': destination}, index = [orderNo])
-        output = output.append(append)
+        #creates a dataframe and appends to a series
+        append = pd.DataFrame({'Shipping #': None, 'Date': dateReceived, 'Product # Quantity': modelAndQuantity, 'Destination': destination, 'Cost': cost}, index = [orderNo])
+        orderConfirmations = orderConfirmations.append(append)
 
     #Checks if it is a "Your order, "BBY01-[NUMBER]" has shipped," in which case can derive the tracking number. 
     elif "Your order" in msg.subject:
-        print("hi")
-        i = 1+1
-        bestBuy.findTrackingNumber(msgHtml)
 
-output.to_csv(r'orderConfirmations.csv')
+        trackingNo, orderNo = bestBuy.findTrackingNumber(msgHtml)
+        try:
+            found = orderConfirmations.loc[orderNo]
+            orderConfirmations = orderConfirmations.drop(orderNo)
+            found.update(pd.Series([trackingNo], index = ['Shipping #']))
+            shippedOrders = shippedOrders.append(found)
+        except KeyError:
+            pass
+
+
+
+shippedOrders.to_csv(r'shippedOrders.csv')
+orderConfirmations.to_csv(r'orderConfirmations.csv')
